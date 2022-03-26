9d0feadad10079802fcfdc9c7d7419c5e1891876
Bruna Gonçalves <bruna.goncalves@luizalabs.com>
2022-03-26 13:37:26 -0300

    fix: fix getById route

--- src/api/customer/customer-route.ts
+++ src/api/customer/customer-route.ts
@@ -5,6 +5,6 @@ const route = Router();
 
 route.post('/v1/customers', ...createCustomer());
 route.get('/v1/customers', ...getCustomersAll());
-route.get('v1/customers/:id', ...getCustomerById());
+route.get('/v1/customers/:id', ...getCustomerById());
 
 export default route;
