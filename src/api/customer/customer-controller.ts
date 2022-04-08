6f105965bfa1c7a0e0ac245b3348184c1bf5d796
Leandro Pereira da Cruz <leandropereiradacruz@gmail.com>
2022-04-08 18:16:01 -0300

    feat: missing commit of 23848

--- src/api/customer/customer-controller.ts
+++ src/api/customer/customer-controller.ts
@@ -1,20 +1,64 @@
-import { CREATED } from "http-status";
-import { persistCustomer } from "./customer-business";
+import { CREATED, NOT_FOUND, OK } from "http-status";
+import {
+  getAllCustomersBusiness,
+  getCustomerById,
+  persistCustomer,
+} from "./customer-business";
 import { createCustomerDeserializer } from "./customer-deserializer";
-import { createCustomerSerializer } from "./customer-serializer";
-import { CustomerCreationRequestHandler } from "./customer-type";
-import { createCustomerValidator } from "./customer-validator";
+import {
+  createCustomerSerializer,
+  getAllCustomersSerializer,
+  getCustomerSerializer,
+  paginationSerializer,
+} from "./customer-serializer";
+import {
+  CustomerCreationRequestHandler,
+  GetAllCustomerRequestHandler,
+  GetCustomerRequestHandler,
+} from "./customer-type";
+import {
+  createCustomerValidator,
+  getAllCustomersValidator,
+  getCustomerValidator,
+} from "./customer-validator";
 
 const createCustomer = (): CustomerCreationRequestHandler[] => {
-    return [
-        createCustomerValidator(),
-        createCustomerDeserializer,
-        persistCustomer,
-        createCustomerSerializer,
-        (req, res) => { res.status(CREATED).json(res.locals.customerToRespond) }
-    ]
-}
-
-export {
-    createCustomer
+  return [
+    createCustomerValidator(),
+    createCustomerDeserializer,
+    persistCustomer,
+    createCustomerSerializer,
+    (req, res) => {
+      res.status(CREATED).json(res.locals.customerToRespond);
+    },
+  ];
 };
+
+const getAllCustomers = (): GetAllCustomerRequestHandler[] => {
+  return [
+    getAllCustomersValidator(),
+    paginationSerializer,
+    getAllCustomersBusiness,
+    getAllCustomersSerializer,
+    (req, res) => {
+      res.status(OK).json(res.locals.customersToRespond);
+    },
+  ];
+};
+
+const getCustomersById = (): GetCustomerRequestHandler[] => {
+  return [
+    getCustomerValidator(),
+    getCustomerById,
+    getCustomerSerializer,
+    (req, res) => {
+      if (!res.locals.customerToRespond) {
+        res.status(NOT_FOUND).json(res.locals.customerToRespond);
+      } else {
+        res.status(OK).json(res.locals.customerToRespond);
+      }
+    },
+  ];
+};
+
+export { createCustomer, getAllCustomers, getCustomersById };
