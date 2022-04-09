8c7e88929110af82af72cc01991703360e46ea74
Leandro Pereira da Cruz <leandropereiradacruz@gmail.com>
2022-04-09 11:31:18 -0300

    feat: missing commit of 23861

--- src/api/customer/customer-serializer.ts
+++ src/api/customer/customer-serializer.ts
@@ -1,4 +1,4 @@
-import { CustomerCreationRequestHandler } from "./customer-type";
+import { CustomerCreationRequestHandler, GetCustomersAllRequestHandler, GetCustomerByIdRequestHandler } from "./customer-type";
 
 const createCustomerSerializer: CustomerCreationRequestHandler = (req, res, next) => {
     const { customerCreated } = res.locals;
@@ -19,6 +19,72 @@ const createCustomerSerializer: CustomerCreationRequestHandler = (req, res, next
     next();
 }
 
+const paginationSerializer: GetCustomersAllRequestHandler = (req, res, next) => {
+    const PaginationParams = req.query;
+    let offset = 0;
+    let limit = 50;
+  
+    if (PaginationParams.offset) {
+      offset = Number(PaginationParams.offset);
+    }
+  
+    if (PaginationParams.limit) {
+      limit = Number(PaginationParams.limit);
+    }
+  
+    res.locals.paginationParamsSerializer = { offset, limit };
+  
+    next();
+};
+
+const getAllCustomersSerializer: GetCustomersAllRequestHandler = (req, res, next) => {
+    const customersAll = res.locals.getCustomer;
+    res.locals.customerToRespond = [];
+
+    customersAll.map((customers) => {
+        res.locals.customerToRespond.push({
+            uuid: customers.uuid,
+            name: customers.name,
+            contact: {
+                email: customers.email,
+                phone: customers.phone,
+            },
+            document: {
+                cpf: customers.cpf,
+                cnpj: customers.cnpj,
+            },
+            createdAt: customers.createdAt.toISOString(),
+            updatedAt: customers.updatedAt.toISOString(),
+        });
+    });
+
+    next();
+};
+
+const getCustomerByidSerializer: GetCustomerByIdRequestHandler = (req, res, next) => {
+    
+    const customer = res.locals.getCustomer;
+
+    if (customer) {
+        res.locals.customerToRespond = {
+            uuid: customer.uuid,
+            name: customer.name,
+            contact: {
+              email: customer.email,
+              phone: customer.phone,
+            },
+            document: {
+              cpf: customer.cpf,
+              cnpj: customer.cnpj,
+            },
+            createdAt: customer.createdAt.toISOString(),
+            updatedAt: customer.updatedAt.toISOString(),
+        };
+    }
+    
+    next();
+};
+
 export {
-    createCustomerSerializer
+    createCustomerSerializer, paginationSerializer, getAllCustomersSerializer, getCustomerByidSerializer
 }
---
+++ src/util/paginationHelpers.ts
@@ -0,0 +1,10 @@
+export type PaginationParams = {
+    offset: string;
+    limit: string;
+};
+  
+export type PaginationParamsSerializer = {
+    offset: number;
+    limit: number;
+};
+  
